e0d869ffd42c341c9990445606598ed21706433c
Jandre Erasmus <54018618+JandreErasmus@users.noreply.github.com>
2020-09-30 11:27:08 +0200

    Add files via upload

---
+++ CMPG215_Encryption_Alpha2.0/CMPG215_Encryption_Alpha2.0/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace CMPG215_Encryption_Alpha2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radt = new System.Windows.Forms.RadioButton();
+            this.radz = new System.Windows.Forms.RadioButton();
+            this.radp = new System.Windows.Forms.RadioButton();
+            this.radj = new System.Windows.Forms.RadioButton();
+            this.txtOther = new System.Windows.Forms.TextBox();
+            this.radOther = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(660, 146);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(92, 32);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse..";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(507, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "OR select a file";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(53, 126);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(430, 150);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.Text = "";
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Type a message:";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.Location = new System.Drawing.Point(53, 301);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(84, 45);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.Location = new System.Drawing.Point(165, 301);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(84, 45);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 376);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Status";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(92, 38);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(303, 26);
+            this.txtKey.TabIndex = 0;
+            this.txtKey.TextChanged += new System.EventHandler(this.txtKey_TextChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(277, 301);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 45);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete...";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(399, 301);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(84, 64);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save File...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.txtStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(53, 403);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(658, 133);
+            this.txtStatus.TabIndex = 12;
+            this.txtStatus.Text = "";
+            this.txtStatus.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radOther);
+            this.groupBox1.Controls.Add(this.radt);
+            this.groupBox1.Controls.Add(this.radz);
+            this.groupBox1.Controls.Add(this.radp);
+            this.groupBox1.Controls.Add(this.radj);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(522, 218);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(152, 161);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select type of file";
+            // 
+            // radt
+            // 
+            this.radt.AutoSize = true;
+            this.radt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radt.Location = new System.Drawing.Point(15, 111);
+            this.radt.Name = "radt";
+            this.radt.Size = new System.Drawing.Size(44, 21);
+            this.radt.TabIndex = 3;
+            this.radt.TabStop = true;
+            this.radt.Text = ".txt";
+            this.radt.UseVisualStyleBackColor = true;
+            // 
+            // radz
+            // 
+            this.radz.AutoSize = true;
+            this.radz.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radz.Location = new System.Drawing.Point(15, 84);
+            this.radz.Name = "radz";
+            this.radz.Size = new System.Drawing.Size(48, 21);
+            this.radz.TabIndex = 2;
+            this.radz.TabStop = true;
+            this.radz.Text = ".zip";
+            this.radz.UseVisualStyleBackColor = true;
+            // 
+            // radp
+            // 
+            this.radp.AutoSize = true;
+            this.radp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radp.Location = new System.Drawing.Point(15, 57);
+            this.radp.Name = "radp";
+            this.radp.Size = new System.Drawing.Size(54, 21);
+            this.radp.TabIndex = 1;
+            this.radp.TabStop = true;
+            this.radp.Text = ".png";
+            this.radp.UseVisualStyleBackColor = true;
+            // 
+            // radj
+            // 
+            this.radj.AutoSize = true;
+            this.radj.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radj.Location = new System.Drawing.Point(15, 30);
+            this.radj.Name = "radj";
+            this.radj.Size = new System.Drawing.Size(49, 21);
+            this.radj.TabIndex = 0;
+            this.radj.TabStop = true;
+            this.radj.Text = ".jpg";
+            this.radj.UseVisualStyleBackColor = true;
+            // 
+            // txtOther
+            // 
+            this.txtOther.Location = new System.Drawing.Point(680, 356);
+            this.txtOther.Name = "txtOther";
+            this.txtOther.Size = new System.Drawing.Size(100, 20);
+            this.txtOther.TabIndex = 14;
+            this.txtOther.TextChanged += new System.EventHandler(this.txtOther_TextChanged);
+            // 
+            // radOther
+            // 
+            this.radOther.AutoSize = true;
+            this.radOther.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radOther.Location = new System.Drawing.Point(15, 138);
+            this.radOther.Name = "radOther";
+            this.radOther.Size = new System.Drawing.Size(59, 21);
+            this.radOther.TabIndex = 4;
+            this.radOther.TabStop = true;
+            this.radOther.Text = "other";
+            this.radOther.UseVisualStyleBackColor = true;
+            this.radOther.CheckedChanged += new System.EventHandler(this.radOther_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 572);
+            this.Controls.Add(this.txtOther);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "DEWCT Encryption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+  private System.Windows.Forms.Button btnBrowse
+      ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox txtStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radt;
+        private System.Windows.Forms.RadioButton radz;
+        private System.Windows.Forms.RadioButton radp;
+        private System.Windows.Forms.RadioButton radj;
+        private System.Windows.Forms.RadioButton radOther;
+        private System.Windows.Forms.TextBox txtOther;
+    }
+}
+
---
+++ CMPG215_Encryption_Alpha2.0/CMPG215_Encryption_Alpha2.0/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CMPG215_Encryption_Alpha2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string key, sourceFile;
+        public bool flag = false;
+        public bool flagEnc = false;
+        string sty = "";
+
+        public MemoryStream memStream;
+        public CryptoStream cryptStream;
+        public FileStream fileS;        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radj.Checked = true;
+            txtOther.Enabled = false;
+        }
+
+        private void EncryptFile()
+        {
+            try 
+            { 
+                if (flag == true)
+                {
+                    DESCryptoServiceProvider DESCrypt = new DESCryptoServiceProvider();
+                    fileS = File.OpenRead(sourceFile);
+
+                    byte[] bytes = new byte[fileS.Length];
+                    fileS.Read(bytes, 0, (int)fileS.Length);
+                    fileS.Close();
+
+                    byte[] byteKey = Encoding.Default.GetBytes(key);
+                    SHA1 hash = new SHA1Managed();
+                    byte[] byteHash = hash.ComputeHash(byteKey);
+
+                    byte[] Keyb = new byte[8];
+                    byte[] IV = new byte[8];
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Keyb[i] = byteHash[i];
+                    }
+
+                    for (int i = 8; i < 16; i++)
+                    {
+                        IV[i - 8] = byteHash[i];
+                    }
+
+                    DESCrypt.Key = Keyb;
+                    DESCrypt.IV = IV;
+
+                    memStream = new MemoryStream();
+                    cryptStream = new CryptoStream(memStream, DESCrypt.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    cryptStream.Write(bytes, 0, bytes.Length);
+                    cryptStream.FlushFinalBlock();
+
+                    txtStatus.Text += "File encrypted succesfully\n";
+
+                    flagEnc = true;
+
+                    fileS.Close();
+                }
+            }   
+            catch(Exception ex)
+            {
+                MessageBox.Show("ERROR ENCRYPTING FILE: " + ex);
+            }
+        }
+
+        public void EncryptText()
+        {
+            int len = txtKey.TextLength;
+            int mesLen = txtMessage.TextLength;
+            byte[] asc = Encoding.ASCII.GetBytes(txtMessage.Text);
+
+
+            txtMessage.Clear();
+            for(int i = 0; i<mesLen; i++)
+            {
+                txtMessage.Text += (char)(asc[i] + len);
+            }
+                
+            
+
+        }
+
+        private void txtKey_TextChanged(object sender, EventArgs e)
+        {
+            key = txtKey.Text;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            txtMessage.Text = "";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sourceFile = openFileDialog1.FileName;                
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+                EncryptFile();
+            else
+                EncryptText();
+        }
+
+        public void writeFile(MemoryStream ms, string type, string path)
+        {
+            if (!path.Contains("."))
+            {
+                if (radj.Checked)
+                    sty = ".jpg";
+                else if (radp.Checked)
+                    sty = ".png";
+                else if (radz.Checked)
+                    sty = ".zip";
+                else if (radt.Checked)
+                    sty = ".txt";
+                else if (radOther.Checked)
+                    txtOther.Enabled = true;
+
+                path += sty;
+            }
+
+            fileS = File.OpenWrite(path);
+
+            foreach (byte b in ms.ToArray())
+                fileS.WriteByte(b);
+
+            txtStatus.Text += type + " file succesfully written to " + path + "\n";
+
+            fileS.Close();
+                
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+                DecryptFile();
+            else
+                DecryptText();
+
+        }
+
+        public void DecryptText()
+        {
+           int len = txtKey.TextLength;
+            int mesLen = txtMessage.TextLength;
+            byte[] asc = Encoding.ASCII.GetBytes(txtMessage.Text);
+
+            txtMessage.Clear();
+            for (int i = 0; i < mesLen; i++)
+            {
+                txtMessage.Text += (char)(asc[i] - len);
+            }
+
+        }
+
+        public void DecryptFile()
+        {
+            try
+            {
+                DESCryptoServiceProvider DESCrypt = new DESCryptoServiceProvider();
+
+                fileS = File.OpenRead(sourceFile);
+                byte[] bytes = new byte[fileS.Length];
+                fileS.Read(bytes, 0, (int)fileS.Length);
+                fileS.Close();
+
+                byte[] byteKey = Encoding.Default.GetBytes(key);
+                SHA1 hash = new SHA1Managed();
+                byte[] hashByte = hash.ComputeHash(byteKey);
+
+                byte[] Key = new byte[8];
+                byte[] IV = new byte[8];
+
+                for (int i = 0; i < 8; i++)
+                {
+                    Key[i] = hashByte[i];
+                }
+
+                for (int i = 8; i < 16; i++)
+                {
+                    IV[i - 8] = hashByte[i];
+                }
+
+                DESCrypt.Key = Key;
+                DESCrypt.IV = IV;
+
+                memStream = new MemoryStream();
+                cryptStream = new CryptoStream(memStream, DESCrypt.CreateDecryptor(), CryptoStreamMode.Write);
+
+                cryptStream.Write(bytes, 0, bytes.Length);
+                cryptStream.FlushFinalBlock();
+
+                txtStatus.Text += "File decrypted succesfully\n";
+                flagEnc = false;
+
+                fileS.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR DECRYPTING FILE" + ex);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            flag = false;            
+        }
+
+        private void txtOther_TextChanged(object sender, EventArgs e)
+        {
+            sty = txtOther.Text;
+        }
+
+        private void radOther_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radOther.Checked)
+                txtOther.Enabled = true;
+            else
+                txtOther.Enabled = false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.Delete(openFileDialog1.FileName);
+                   txtStatus.Text += openFileDialog1.FileName + " deleted successfully\n";
+                }
+                catch(IOException ex)
+                {
+                    MessageBox.Show("ERROR deleting file: " + ex);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            string type = "";
+
+            if (flagEnc == true)
+            {
+                type = "Encrypted";
+                saveFileDialog1.Title = "Save ENCRYPTED file";
+            }
+                
+            else
+            {
+                type = "Decrypted";
+                saveFileDialog1.Title = "Save DECRYPTED file";
+            }
+                
+
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (flag == true)
+                    writeFile(memStream, type, saveFileDialog1.FileName);
+                else
+                    writeText(saveFileDialog1.FileName);
+            }
+        }
+
+        public void writeText(string path)
+        {
+            StreamWriter sWrite = new StreamWriter(path);
+
+            sWrite.WriteLine(txtMessage.Text);
+
+            txtStatus.Text +=  " Message succesfully written to " + path + "\n";
+
+            sWrite.Close();
+        }
+    }
+}
